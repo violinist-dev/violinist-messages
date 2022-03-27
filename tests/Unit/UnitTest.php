c0f214e84631d8155f7134cad95b3cb7d4bf6ce7
Eirik S. Morland <eirik@morland.no>
2022-03-27 14:14:00 +0200

    Typo

--- tests/Unit/UnitTest.php
+++ tests/Unit/UnitTest.php
@@ -39,7 +39,7 @@ class UnitTest extends TestCase
         $update->setCurrentVersion('1');
         $this->assertEquals('1', $update->getCurrentVersion());
         $update->setChangedFiles(['file1', 'file2']);
-        $this>self::assertEquals(['file1', 'file2'], $update->getChangedFiles());
+        self::assertEquals(['file1', 'file2'], $update->getChangedFiles());
     }
 
     public function testLegacyFormat()
